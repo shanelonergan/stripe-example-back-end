526c719c19707935f22082b7b8a00b97c3223757
shanelonergan <sptlonergan@gmail.com>
2020-02-23 21:13:41 -0500

    fetch is reaching back end

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,15 +1,18 @@
 require 'stripe'
 require 'dotenv'
+Dotenv.load
 
 class ChargesController < ApplicationController
 
     def create
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+        token = params[:charge][:token]
+        price = params[:price]
 
         Stripe::Charge.create({
             amount: 2000,
             currency: 'usd',
-            source: 'tok_visa',
+            source: token,
             description: 'My First Test Charge (created for API docs)',
         })
     end
