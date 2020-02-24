e4224e97c33b870858c6610615fa88e018c532e5
shanelonergan <sptlonergan@gmail.com>
2020-02-23 21:43:20 -0500

    working!

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,16 +5,21 @@ Dotenv.load
 class ChargesController < ApplicationController
 
     def create
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-        token = params[:charge][:token]
-        price = params[:price]
 
-        Stripe::Charge.create({
-            amount: 2000,
-            currency: 'usd',
-            source: token,
-            description: 'My First Test Charge (created for API docs)',
-        })
+        begin
+            Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+            token = params[:charge][:token]
+            price = params[:price]
+
+            charge = Stripe::Charge.create({
+                amount: price,
+                currency: 'usd',
+                source: token,
+                description: 'My First Test Charge (created for API docs)',
+            })
+
+            render json: charge
+        end
     end
 
 end
