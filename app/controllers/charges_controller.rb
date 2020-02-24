c886772ae4da8897125b825bd956884f7f0b7bf1
shanelonergan <sptlonergan@gmail.com>
2020-02-23 20:53:26 -0500

    cors

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,17 @@
-class ChargesController < ApplicationController
-    def create
+require 'stripe'
+require 'dotenv'
 
+class ChargesController < ApplicationController
+
+    def create
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+        Stripe::Charge.create({
+            amount: 2000,
+            currency: 'usd',
+            source: 'tok_visa',
+            description: 'My First Test Charge (created for API docs)',
+        })
     end
+
 end
